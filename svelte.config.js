dd7767e29297447649e18206a851782f8171ec9a
jshiah <joyce.shiah@gmail.com>
2024-11-22 16:51:21 -0800

    Deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+// 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+// 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
+
+import adapter from '@sveltejs/adapter-static';
+
+export default {
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: '', // Adjust if deploying to a subfolder
+    },
+  },
 };
 
-export default config;
